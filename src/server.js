02cc739505df8d10d87e75a8b48b40acdfcc1e17
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-12-21 23:30:31 -0300

    first commmit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, ()=>{console.log("Run in 4000")})
